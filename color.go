94f7c5f5ac0410e1d44ad44127ceb0128bca311f
Takashi Takizawa <taki@cyber.email.ne.jp>
2021-06-20 17:16:08 +0900

    Revert.

---
+++ color.go
@@ -0,0 +1,68 @@
+// Copyright 2021 Takashi Takizawa. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package color
+
+import (
+	"fmt"
+	"os"
+	"strings"
+
+	"github.com/mattn/go-isatty"
+)
+
+const version = "0.1.2"
+
+var terminalDetection = true
+
+// Color is a color for output.
+type Color int
+
+// NewColor creates the Color.
+func NewColor(name string) (Color, error) {
+	for i, v := range colorNames {
+		if strings.EqualFold(v, name) {
+			return Color(i), nil
+		}
+	}
+	return 0, fmt.Errorf("unknown color name: %s", name)
+}
+
+// String returns the name of the color.
+func (c Color) String() string {
+	return colorNames[c]
+}
+
+func (c Color) escapeSequence() string {
+	if 0 <= c && int(c) < len(colorEscapeSequences) {
+		return colorEscapeSequences[c]
+	}
+	return colorEscapeSequences[0]
+}
+
+// Colorize colorizes the string.
+func (c Color) Colorize(str string) string {
+	isTerminal := true
+	if terminalDetection {
+		isTerminal = isatty.IsTerminal(os.Stdout.Fd())
+	}
+	if !isTerminal {
+		return str
+	}
+	return fmt.Sprintf("%s%s%s", c.escapeSequence(), str, Default.escapeSequence())
+}
+
+// EnableTerminalDetection enables terminal detection.
+// If no terminal is detected, colorization is suppressed.
+// By default, the terminal detection is enabled.
+func EnableTerminalDetection() {
+	terminalDetection = true
+}
+
+// DisableTerminalDetection disables terminal detection.
+// Even if the terminal is not detected, colorization is not suppressed.
+// By default, the terminal detection is enabled.
+func DisableTerminalDetection() {
+	terminalDetection = false
+}
